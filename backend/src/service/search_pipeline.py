f90a9ebb9d89c2a2cc3ce574e8f5bf5ac17f2905
202310957 <snu11025@naver.com>
2026-02-20 00:57:28 +0900

    refactor: client를 재사용하도록 로직 수정, kiwi 토크나이저를 사용하여 sparse retriever의 품질 향상 유도

--- backend/src/service/search_pipeline.py
+++ backend/src/service/search_pipeline.py
@@ -2,6 +2,7 @@ from typing import List
 from src.schema.search import SearchResponse
 from src.repository.user_repository import UserRepository
 from src.repository.user_search_log_repository import UserSearchLogRepository
+# import time
 
 class SearchPipeline:
     def __init__(
@@ -18,8 +19,12 @@ class SearchPipeline:
 
     async def initialize_search_session(self, x: float, y: float) -> None:
         self.docs = await self.context_builder_service.build_search_context(x, y)
+        # start_time = time.time()
         await self.faiss_service.add_index(self.docs)
+        # print("식당 정보를 임베딩합니다.:", time.time() - start_time)
+        # start_time = time.time()
         self.search_service.prepare_search_context(self.docs)
+        # print(f"검색을 준비합니다.:", time.time() - start_time)
 
     async def search_places(self, query: str) -> List[SearchResponse]:
         ranked_results = await self.search_service.HybridRetriever(query)
@@ -41,6 +46,11 @@ class SearchPipeline:
         UserSearchLogRepository.save(user_id, query)
     
     async def recommend(self, ip_address: str, x: float, y: float, query: str) -> List[SearchResponse]:
+        # start_time = time.time()
         self.save_logs(ip_address, query)
+        # print("사용자 요청 로그를 저장합니다.:", time.time() - start_time)
         await self.initialize_search_session(x, y)
-        return await self.search_places(query)
+        # start_time = time.time()
+        results = await self.search_places(query)
+        # print("하이브리드 검색을 수행합니다.:", time.time() - start_time)
+        return results
